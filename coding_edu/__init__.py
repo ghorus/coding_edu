b2b15d951b8c92dcf3b1cd5dc3c90ff0255709f5
ghorus <nguyen.victor4@gmail.com>
2025-10-17 14:46:35 -0700

    normal

--- coding_edu/__init__.py
+++ coding_edu/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config["SECRET_KEY"] = os.urandom(24)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-print(os.environ.get("DATABASE_URL"))
+
 db = SQLAlchemy(app)
 
 
